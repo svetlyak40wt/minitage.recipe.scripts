246ce4a7405b0d58f6bcbbf11dc77014fa4d27d6
kiorky <kiorky@cryptelium.net>
2009-07-07 18:24:48 +0200

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setupdir = os.path.abspath(
 os.chdir(setupdir)
 
 name='minitage.recipe'
-version = '1.18'
+version = '1.19'
 
 def read(*rnames):
     return open(
--- src/minitage/recipe/egg/__init__.py
+++ src/minitage/recipe/egg/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2009, Mathieu PASQUET <kiorky@cryptelium.net>
+# -*- coding: utf-8 -*-
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
